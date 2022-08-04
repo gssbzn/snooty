08552c09f0ac2c4d34a5d4625f3199dcbda525bb
Anabella Buckvar <ab745@duke.edu>
2022-08-04 07:47:33 -0400

    Fixed bug on third screen

--- src/components/Widgets/FeedbackWidget/context.js
+++ src/components/Widgets/FeedbackWidget/context.js
@@ -98,8 +98,9 @@ export function FeedbackProvider({ page, hideHeader, test = {}, ...props }) {
   */
 
   async function submitAllFeedback({ comment = '', email = '' }) {
-    setView('submitted');
     setProgress([true, true, true]);
+
+    setView('submitted');
     if (!selectedSentiment) return;
     // Submit the full feedback document
     const segment = getSegmentUserId();
